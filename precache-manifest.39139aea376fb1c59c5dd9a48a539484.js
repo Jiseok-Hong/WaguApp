ff3b4a21e0a766c8b17894f42c08b74282f50652
Jiseok-Hong <ji385@naver.com>
2020-09-05 17:10:44 +0800

    Updates

---
+++ precache-manifest.39139aea376fb1c59c5dd9a48a539484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda1f787304b9ac1680f5599c19093a5",
+    "url": "/WaguApp/index.html"
+  },
+  {
+    "revision": "11e37421bd89cf8fbee0",
+    "url": "/WaguApp/static/js/2.548da76c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WaguApp/static/js/2.548da76c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cfb866337eb46467cf3",
+    "url": "/WaguApp/static/js/main.c27ca486.chunk.js"
+  },
+  {
+    "revision": "9d0ff2f877e9f0e51f27",
+    "url": "/WaguApp/static/js/runtime-main.5dfdc7e2.js"
+  }
+]);
